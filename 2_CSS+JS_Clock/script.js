7ab16b6775eb42fbf891b48f31277c4091612f31
Artiom_Sokolov <sokolova-net@mail.ru>
2022-02-19 18:56:01 +0300

    comment added to task 2

--- 2_CSS+JS_Clock/script.js
+++ 2_CSS+JS_Clock/script.js
@@ -10,11 +10,11 @@ function setDate() {
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
     const mins = now.getMinutes();
-    const minsDegrees = ((mins / 60) * 360) + ((seconds / 60) * 6) + 90;
+    const minsDegrees = ((mins / 60) * 360) + ((seconds / 60) * 6) + 90; // без ((seconds / 60) * 6) минутная стрелка будет двигаться прямо из одной точки в другую. умножаю на 6, потому что каждая прошедшая секунда составляла 6° на часах
     minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 
     const hour = now.getHours();
-    const hourDegrees = ((hour / 12) * 360) + ((mins / 60) * 30) + 90;
+    const hourDegrees = ((hour / 12) * 360) + ((mins / 60) * 30) + 90; // так же, как и в минутах, только максимальный градус, на который может вращаться минутная стрелка, составляет 30 градусов, рассчитанный из 360 градусов / 12 часов.
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
